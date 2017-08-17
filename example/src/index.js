30ad62b78ace7d0a3359d8a6befa0b24e2dea249
Colin Ritchey <colinritchey256@gmail.com>
2017-08-17 16:03:19 -0700

    Iniital commit

---
+++ example/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import ListPage from './components/ListPage'
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo'
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const networkInterface = createNetworkInterface({
+// __SIMPLE_API_ENDPOINT__ looks similar to: `https://api.graph.cool/simple/v1/<PROJECT_ID>`
+  uri: 'https://api.graph.cool/simple/v1/cj4syt7vh488j0179wg0c7qn7'
+})
+
+const client = new ApolloClient({networkInterface})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path='/' component={ListPage} />
+      </div>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
