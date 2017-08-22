ee11db3be86ca655bcff26d16c9b80a0f5fda9cc
Colin Ritchey <colinritchey256@gmail.com>
2017-08-21 17:18:40 -0700

    Styling changes to ListPage

--- example/src/components/ListPage/ListPage.js
+++ example/src/components/ListPage/ListPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Post from '../Post/Post'
 import { gql, graphql } from 'react-apollo'
+import './styles.css'
 
 class ListPage extends React.Component {
 
@@ -14,7 +15,7 @@ class ListPage extends React.Component {
   render() {
     if (this.props.data.loading) {
       return (
-        <div className='flex w-100 h-100 items-center justify-center pt7'>
+        <div className='loading'>
           <div>
             Loading
             (from {process.env.REACT_APP_GRAPHQL_ENDPOINT})
@@ -30,8 +31,8 @@ class ListPage extends React.Component {
     // }
 
     return (
-      <div className={'w-100 flex justify-center pa6'}>
-        <div className='posts' style={{maxWidth: 1150}}>
+      <div className='listPage-container'>
+        <div className='post-list'>
           <Link
             to='/create'
             className='ma3 box new-post br2 flex flex-column items-center justify-center ttu fw6 f20 black-30 no-underline'
