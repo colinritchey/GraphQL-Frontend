a7a4a42e2c2e0fb255901f287eb9520a029fc0c9
Colin Ritchey <colinritchey256@gmail.com>
2017-09-15 16:11:59 -0700

    axis bar lines added, d3 in package.json

--- example/src/components/BarChart/BarChart.js
+++ example/src/components/BarChart/BarChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { scaleLinear } from 'd3-scale';
+import * as d3 from "d3";
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
 
@@ -20,11 +21,20 @@ class BarChart extends Component {
   createBarChart() {
     const node = this.node
     const dataMax = max(this.props.data)
+    const xScale = scaleLinear().range([0, this.props.size[0]]);
+
     const yScale = scaleLinear()
       .domain([0, dataMax])
-      .range([0, this.props.size[1]])
+      .range([0, 300])
+
+    const y = d3.scaleLinear()
+      .domain([0, dataMax])
+      .range([this.props.size[1], 0]);
 
     select(node)
+      .append('g')
+      .attr("transform", "translate(30, 30)")
+
       .selectAll('rect')
       .data(this.props.data)
       .enter()
@@ -38,12 +48,33 @@ class BarChart extends Component {
 
     select(node)
       .selectAll('rect')
+
       .data(this.props.data)
       .style('fill', '#fe9922')
       .attr('x', (d,i) => i*35)
       .attr('y', d => (this.props.size[1] - yScale(d)))
       .attr('height', d => yScale(d))
       .attr('width', 25)
+
+    select(node)
+      .append("g")
+      .attr("transform", "translate(30," + (this.props.size[1]+30) + ")")
+      .call(d3.axisBottom(xScale));
+
+    select(node)
+      .append("g")
+      .attr("transform", "translate(" + (this.props.size[0]+30) + ", 30)")
+      .call(d3.axisRight(y));
+
+    select(node)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", this.props.size[1] + 50)
+      .attr("x", 0 - (this.props.size[0]/2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Value");
+
   }
 
   render() {
@@ -53,4 +84,4 @@ class BarChart extends Component {
    }
 }
 
-export default BarChart
+export default BarChart;
